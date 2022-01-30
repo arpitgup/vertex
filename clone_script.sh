86ba975ee10dc8c9ff579f3224a0b1e237bf3d26
arpitgup <92305778+arpitgup@users.noreply.github.com>
2022-01-30 21:02:38 +0530

    Delete clone_script.sh

--- clone_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-# Cloning ipynb files from github
-
-git clone https://github.com/arpitgup/vertex.git
-cd vertex
-pip install runipy
-runipy Fraud_Transaction_notebook.ipynb OutputNotebook.ipynb
-sudo su
-cp /opt/c2d/vertex/*.ipynb /home/jupyter
